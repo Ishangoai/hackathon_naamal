a2305eaff60a0f2e5d93fa5ba4ab8725eff266ee
owuraku-zenas <zenasawuku@gmail.com>
2023-10-12 16:50:21 +0000

    added types to the .to_sql method

--- src/DataSource/main.py
+++ src/DataSource/main.py
@@ -1,8 +1,7 @@
 import pandas as pd
 from decouple import config
 from sqlalchemy import create_engine
-from cleaning_utils import column_name_mapping
-
+from cleaning_utils import column_name_mapping, data_types
 
 sheet_id = config("SPREADSHEET_ID")
 db_url = config('DATABASE_URL')
@@ -16,6 +15,6 @@ print(df)
 
 
 engine = create_engine(db_url, echo=True)
-df.to_sql('alumni', con=engine, if_exists='replace', index=False)
+df.to_sql('alumni', con=engine, if_exists='replace', index=False, dtype=data_types)
 
 engine.dispose()
